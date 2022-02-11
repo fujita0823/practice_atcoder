57a0187c836b3dfb80409a0f85d0a1913cab50fa
fujita0823 <fujita@DESKTOP-C3LS1U4.localdomain>
2022-02-11 13:32:28 +0900

    make folders

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/fujita0823/practice_atcoder.git
+git push -u origin main
